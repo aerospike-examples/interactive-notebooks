0befe2961a3cf529142c71b56845dd1febf183a6
Joe Martin <joe.martin@gmail.com>
2020-11-24 20:39:59 -0800

    Some small changes to tweetaspike. A lot of the code should be removed.

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -2,4 +2,4 @@ rm -rf target
 mkdir target
 cp -r docker/* target
 cp -r java python target/notebooks/
-docker build -t ${1:-aerospike/aerospike-nb} target
+docker build -t ${1:-aerospike/intro-notebooks} target
