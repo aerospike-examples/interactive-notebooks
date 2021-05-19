fd7ffd6eced8983a4709fa3d9fdd40381d43901c
Lucien Volmar <lucien@aerospike.com>
2021-05-19 13:33:22 -0700

    Aerospike 5.6.0.3

--- bin/build-docker-image.sh
+++ bin/build-docker-image.sh
@@ -1,7 +1,7 @@
 rm -rf target
 mkdir target
 cp -r docker/* target
-cp -r notebooks/java target/notebooks/
-cp -r notebooks/python target/notebooks/
+#cp -r notebooks/java target/notebooks/
+#cp -r notebooks/python target/notebooks/
 
 docker build --no-cache -t ${1:-aerospike/intro-notebooks} target
