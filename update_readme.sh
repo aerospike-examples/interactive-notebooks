d294e0f1dbadc22c77787c600d116e8f7079f8f4
neelp-git <nphadnis@aerospike.com>
2021-05-27 09:44:49 -0700

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -8,7 +8,7 @@ HEADER_TEXT=$( cat <<"END-HEADER-TEXT"
 
 This area is for [Jupyter notebooks](https://jupyter.org/) in .ipynb format. Python and Java notebooks are currently supported by the kernel. 
 
-The list of notebooks below has links to browse each notebook in the viewer and to launch it in interactive mode in Binder. This repository also provides a Docker container that you can install (see the instructions) to run the notebooks locally. 
+The list of notebooks below has links to browse each notebook in the viewer and to launch it in interactive mode in Binder. This repository also provides a Docker container that you can install (see the [instructions](../README.md)) to run the notebooks locally. 
 END-HEADER-TEXT
 )
 
