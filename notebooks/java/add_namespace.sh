03e826fe1369b216dfd3df63af8fad6c68279ad4
Neel Phadnis <nphadnis@aerospike.com>
2022-04-12 11:18:05 -0700

    Update add_namespace.sh

--- notebooks/java/add_namespace.sh
+++ notebooks/java/add_namespace.sh
@@ -2,4 +2,4 @@
 cp /etc/aerospike/aerospike.conf ~/notebooks/java/aerospike.conf
 sed -i '/paxos-single-replica-limit/d' ~/notebooks/java/aerospike.conf
 echo "namespace $1 {\n memory-size 1G \n}" >> ~/notebooks/java/aerospike.conf
-/etc/init.d/aerospike stop; asd --config-file ~/notebooks/java/aerospike.conf
+pkill asd; asd --config-file ~/notebooks/java/aerospike.conf
